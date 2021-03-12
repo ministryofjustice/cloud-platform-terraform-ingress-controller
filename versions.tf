a569622f3cbc34ab769789312c6258b4c81f5046
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-03-12 19:05:59 +0000

    Evaluate the repository with terraform0.13upgrade binary
    In our vision to upgrade Terraform to 0.13, we will need to run this binary over any Terraform files in our repository.

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+  required_version = ">= 0.13"
+}
