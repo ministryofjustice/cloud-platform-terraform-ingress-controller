cf4f9babcf974f17a74727c22734c52469ab4137
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-12-05 20:39:17 +0000

    up chart ver

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "helm_release" "nginx_ingress" {
   chart      = "ingress-nginx"
   namespace  = kubernetes_namespace.ingress_controllers.id
   repository = "https://kubernetes.github.io/ingress-nginx"
-  version    = "3.34.0"
+  version    = "4.0.12"
 
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
     metrics_namespace       = kubernetes_namespace.ingress_controllers.id
