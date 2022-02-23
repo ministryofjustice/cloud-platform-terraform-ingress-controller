34221177009f2ce9b34a9e1a023bd62710c9c91f
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-02-23 09:09:38 +0000

    typo

--- example/ingress.tf
+++ example/ingress.tf
@@ -1,7 +1,7 @@
 module "ingress_controllers" {
-  source = "../
+  source = "../"
 
-  replica_count       = "1"
+  replica_count       = "2"
   controller_name     = "default"
   cluster_domain_name = "dummy"
   is_live_cluster     = false
@@ -17,7 +17,7 @@ module "ingress_controllers" {
 module "modsec_ingress_controllers" {
   source = "../"
 
-  replica_count       = "1"
+  replica_count       = "2"
   controller_name     = "modsec"
   cluster_domain_name = "dummy"
   is_live_cluster     = false
