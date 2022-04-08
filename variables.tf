c2c1f75fdde8817d2a49a38b582e4a63b3506b72
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-04-08 11:35:19 +0100

    typo

--- variables.tf
+++ variables.tf
@@ -26,6 +26,7 @@ variable "backend_repo" {
 variable "backend_tag" {
   description = "tag of the default backend app"
   default     = "0.6"
+}
 
 variable "replica_count" {
   type        = string
