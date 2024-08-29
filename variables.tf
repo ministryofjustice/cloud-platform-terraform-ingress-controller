e5c814fedd54ecf4a81604444580d17ddbb54905
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-08-29 12:47:44 +0100

    chore: add variable to control namespace createion

--- variables.tf
+++ variables.tf
@@ -80,6 +80,11 @@ variable "eip_allocation_annotation" {
   default     = ""
 }
 
+variable "create_namespace" {
+  description = "Create ingress namespace"
+  type        = bool
+  default     = false
+}
 
 variable "keepalive" {
   description = "the maximum number of idle keepalive connections to upstream servers that are preserved in the cache of each worker process. When this number is exceeded, the least recently used connections are closed. https://nginx.org/en/docs/http/ngx_http_upstream_module.html#keepalive"
