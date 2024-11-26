dfbb26e3be395e44eaf434b92d207dcc3eb8cd13
Jaskaran Sarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-11-26 14:30:29 +0000

    style: 💄 pulldebug level back into log (#113)

--- configmap.tf
+++ configmap.tf
@@ -193,10 +193,10 @@ resource "kubernetes_config_map" "fluent-bit-config" {
         Time_Key    time
         Time_Format %Y-%m-%dT%H:%M:%S.%L%z
     [PARSER]
-        # https://rubular.com/r/DjPmoX5HnQMesk
+        # https://rubular.com/r/HSac4mikEZ3iF8
         Name modsec-debug-logs
         Format regex
-        Regex ^(?<debug_uid>\[\d+\.\d+\]) (?<uri>\[.*\]) (?<log>.*)$
+        Regex ^(?<debug_uid>\[\d+\.\d+\]) (?<uri>\[.*\]) (?<log>\[\d+\].*)$
         Time_Key    time
         Time_Format %Y-%m-%dT%H:%M:%S.%L%z
     [PARSER]
